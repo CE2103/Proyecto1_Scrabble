295464d5b9dd4298c8eb3ac96275a551c7b1c2a0
angelortizv <angelo2898@gmail.com>
2019-03-19 01:16:38 -0600

    test

---
+++ cmake-build-debug/CMakeFiles/Proyecto1_Scrabble.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto1_Scrabble.dir/main.cpp.o"
+  "Proyecto1_Scrabble.pdb"
+  "Proyecto1_Scrabble"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto1_Scrabble.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
