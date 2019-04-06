84fd72c283a41f711700cafe07b4276fcc261e05
Angelo Ortiz Vega <angelo2898@gmail.com>
2019-04-06 00:16:12 -0600

    Code cleaning to receive something new [gui]

--- main.cpp
+++ main.cpp
@@ -1,59 +1,8 @@
 #include <iostream>
 
-#include "util/dictionary.h"
-
-#include "dataStructures/linkedList/list.h"
-#include "dataStructures/linkedList/list.cpp"
-
-
 using namespace std;
 
 
 int main() {
-
-    string letra="";
-
-    List<string> d;
-
-    d.add_head("arbol");
-    d.add_head("carro");
-    d.add_head("perro");
-    d.add_head("casa");
-    d.add_head("caballo");
-
-    List<string> lista;
-
-    cout<< "ingrese una palabra";
-    cin >> letra;
-    lista.add_head(letra);
-    cout<< "ingrese una palabra";
-    cin >> letra;
-    lista.add_end(letra);
-    cout<< "ingrese una palabra" ;
-    cin >> letra;
-    lista.add_end(letra);
-    cout<< "ingrese una palabra" ;
-    cin >> letra;
-    lista.add_end(letra);
-    cout<< "ingrese una palabra" ;
-    cin >> letra;
-    lista.add_end(letra);
-
-
-    string compara= lista.Unir();
-    if (d.search(compara)){
-        cout << "La palabra se encuentra en el diccionario"<< endl;
-    }
-    else{
-        lista.invert();
-        string compara2= lista.Unir();
-
-        if (d.search(compara2)){
-            cout << "La palabra se encuentra en el diccionario"<< endl;
-        }
-        else {
-            cout << "La palabra NO se encuentra en el diccionario"<< endl;
-        }
-    }
     return 0;
 }
