47d10e3f7a8792d38b5f2335822922a4fe1bac50
Angelo Ortiz Vega <angelo2898@gmail.com>
2019-04-08 18:15:56 -0600

    thirddialog changes

--- principal/thirddialog.h
+++ principal/thirddialog.h
@@ -8,6 +8,7 @@
 
 #include <QDialog>
 #include <iostream>
+#include <communication/socketClient.h>
 
 using namespace std;
 
@@ -23,16 +24,22 @@ public:
     explicit ThirdDialog(QWidget *parent = 0);
     ~ThirdDialog();
 
+
+
 private slots:
     void on_pushButton_4_clicked();
 
     void on_pushButton_2_clicked();
 
     void on_pushButton_3_clicked();
+    void printMensaje(QString msn);
+    void sendMensaje();
 
 
 private:
     Ui::ThirdDialog *ui;
+    SocketCliente *conexion;
+
 };
 
 #endif //PROYECTO1_SCRABBLE_THIRDDIALOG_H
